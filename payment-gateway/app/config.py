6291173c0005429dfc8eca895c63f5b4aa3bd180
AlexanderNino <wilson.nino02@uptc.edu.co>
2026-05-19 16:38:45 -0500

    Implementar NU en pasarela de pagos

--- payment-gateway/app/config.py
+++ payment-gateway/app/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     VISA_SERVICE_URL: str
     MASTERCARD_SERVICE_URL: str
+    NU_SERVICE_URL: str = "https://nu-service.onrender.com"
+    NU_TOKEN: int = 2000
     APP_ENV: str = "development"
 
     class Config:
